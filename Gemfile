13c7ba4521e37dae595099b0e6ef9542510dd640
Ed Jefferson <e.jefferson@gmail.com>
2013-03-12 11:27:17 +0000

    fixed since_id bug

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem 'mysql'
 gem 'twitter'
 gem 'engtagger'
+gem 'foreman'
--- georgedebotton.rb
+++ georgedebotton.rb
@@ -73,16 +73,17 @@ x = result.fetch_row
 
 
 
-  LatestTweet = AlainTweets.search("from:alaindebotton", :result_type => "recent",  :since_id => 1 #x[0].to_i 
+  LatestTweet = AlainTweets.search("from:alaindebotton", :result_type => "recent",  :since_id => x[0].to_i 
   ).results.reverse.each do |status|
     puts status.id
     tweettext = status.text.georgealyser
     puts tweettext
     tweetid=status.id
+    puts con
     con.query("update lasttweet set lasttweet=#{tweetid} where id=1")
     
     if tweettext!="no nouns"
-       #AlainTwoots.update(tweettext.trim140)  
+       AlainTwoots.update(tweettext.trim140)  
     end
     
   
